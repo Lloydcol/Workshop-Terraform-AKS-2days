5e8e0154e42bfce64abadb2c4d0942babc1a1332
Colart Lloyd <55253106+Lloydcol@users.noreply.github.com>
2022-07-06 16:10:16 +0100

    Update 1-versions.tf

--- Terraform/Lab4_Pipeline_CI_CD_Terraform/1-versions.tf
+++ Terraform/Lab4_Pipeline_CI_CD_Terraform/1-versions.tf
@@ -12,7 +12,7 @@ terraform {
   # cf. https://www.terraform.io/language/settings/backends/azurerm
   backend "azurerm" {
     resource_group_name  = "RG-AKSCluster-Lloyd"         # mettre ici le nom du resource group de vos ressource
-    storage_account_name = "backendterralloyd"      # mettre le nom du compte de stockage créer dans le lab 1
+    storage_account_name = "terraformlloyd"      # mettre le nom du compte de stockage créer dans le lab 1
     container_name       = "tfstate"
     key                  = "pipeline-gh.terraform.tfstate" 
     # pour les options d'authentification au backend Azure Storage cf. https://www.terraform.io/language/settings/backends/azurerm
